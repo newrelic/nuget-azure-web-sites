2b1672a142569a25807373b7f037f7bca848321d
Nick Floyd <nickfloyd@newrelic.com>
2013-04-09 00:43:33 -0700

    dotnet-369 : adds static tests to verify newrelic.config

--- nuget.test/tests/package_content.Tests.ps1
+++ nuget.test/tests/package_content.Tests.ps1
@@ -9,11 +9,11 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 		}
 		
 		It "checks to see if content\newrelic\SampleClassLibrary.dll exists" {
-			(Test-Path $PackageRoot\content\newrelic\SampleClassLibrary.dll) | Should Be $true
+			(Test-Path $PackageRoot\content\newrelic\NewRelic.Agent.Core.dll) | Should Be $true
 		}
 		
 		It "checks to see if content\newrelic\TestProfiler.dll exists" {
-			(Test-Path $PackageRoot\content\newrelic\TestProfiler.dll) | Should Be $true
+			(Test-Path $PackageRoot\content\newrelic\NewRelic.Profiler.dll) | Should Be $true
 		}
 		
 		It "checks to see if content\newrelic\extensions\CoreInstrumentation.xml exists" {
@@ -24,4 +24,31 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 			(Test-Path $PackageRoot\content\newrelic\extensions\extension.xsd) | Should Be $true
 		}
 		
+		Context "When newrelic.config has not been applied to the application" {
+			[xml] $configXml = Get-Content $PackageRoot\content\newrelic.config
+			$ns = @{ e = "urn:newrelic-config" }
+			$ns = New-Object Xml.XmlNamespaceManager $configXml.NameTable
+			$ns.AddNamespace( "e", "urn:newrelic-config" )
+
+		
+			It "Should have the static value REPLACE_WITH_LICENSE_KEY" {
+				$node = $configXml.configuration.service
+				$node.licenseKey | Should be "REPLACE_WITH_LICENSE_KEY"
+			}
+			
+			It "Should have the static value My Application for Application / Name" {
+				$node = $configXml.SelectSingleNode("//e:application[e:name/text()]", $ns)
+				$node.name | Should Be "My Application"
+			}
+			
+			It "Should have agentEnabled set to true" {
+				$node = $configXml.configuration
+				$node.agentEnabled | Should be "true"
+			}
+			
+			It "Should have ssl set to true" {
+				$node = $configXml.configuration.service
+				$node.ssl | Should be "true"
+			}
+		}
     }

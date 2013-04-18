a768aa5937ee1e42e01067a9db521d5b4ca9d5f6
Nick Floyd <nickfloyd@newrelic.com>
2013-04-18 16:01:33 -0700

    updates with an release build

--- nuget.test/tests/package_content.Tests.ps1
+++ nuget.test/tests/package_content.Tests.ps1
@@ -4,7 +4,7 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
 		
-		$agentVersion = "2.6.161.0"
+		$agentVersion = "2.6.169.0"
 		
 		Context "When package exists" {
 			It "checks to see if content\newrelic.config exists" {
--- nuget.test/tests/package_tools.Tests.ps1
+++ nuget.test/tests/package_tools.Tests.ps1
@@ -5,16 +5,15 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
     Describe "package_tools" {
 		
 		It "checks to see if tools\install.ps1 exists" {
-			#Write-Host $packageDir\tools\install.ps1
 			(Test-Path $PackageRoot\tools\install.ps1) | Should Be $true
 		}
 		
 		It "checks to see if tools\uninstall.ps1 exists" {
-			(Test-Path $PackageRoot\tools\install.ps1) | Should Be $true
+			(Test-Path $PackageRoot\tools\uninstall.ps1) | Should Be $true
 		}
 		
 		It "checks to see if tools\NewRelicHelper.psm1 exists" {
-			(Test-Path $PackageRoot\tools\install.ps1) | Should Be $true
+			(Test-Path $PackageRoot\tools\NewRelicHelper.psm1) | Should Be $true
 		}
 		
     }

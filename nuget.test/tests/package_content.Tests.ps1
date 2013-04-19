32141ee4c68366465e07cb13b8436f63fe9f475d
Nick Floyd <nickfloyd@newrelic.com>
2013-04-19 15:28:46 -0700

    new build

--- nuget.test/tests/package_content.Tests.ps1
+++ nuget.test/tests/package_content.Tests.ps1
@@ -4,7 +4,7 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
 		
-		$agentVersion = "2.6.175.0"
+		$agentVersion = "2.6.178.0"
 		
 		Context "When package exists" {
 			It "checks to see if content\newrelic.config exists" {

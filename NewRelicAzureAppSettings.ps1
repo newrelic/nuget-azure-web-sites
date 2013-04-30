8a878a71fe4af3aa3c388e5e9f2f41df045bd214
Nick Floyd <nickfloyd@newrelic.com>
2013-04-30 13:54:00 -0700

    adds information on using the app settings script

--- NewRelicAzureAppSettings.ps1
+++ NewRelicAzureAppSettings.ps1
@@ -1,3 +1,25 @@
+<#
+The purpose of this script is to allow the automation or ease of implementation for deploying
+New Relic to Windows Azure Web Sites
+
+Requirements:
+1. Install the powershell SDK: http://go.microsoft.com/?linkid=9811175&clcid=0x409
+2. Add your Publish Profile:
+https://manage.windowsazure.com >> Web Sites >> [Web Site Name] >> Download the publish profile
+
+PS> azure account download [Subscription Name]
+PS> azure account import [PathToPublishSettingsFile]
+
+3. Run the script:
+PS> .\NewRelicAzureAppSettings.ps1 [MyWebSiteName]
+
+Note: 
+#1 and #2 will only need to be done once for a given Publish Settings file until the file expires.
+#3 only needs to be done once per new Web Site
+
+
+#>
+
 Param(
   [string]$appName
 )

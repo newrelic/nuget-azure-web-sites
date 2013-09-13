36ebb005747ea1308fa764f087a359f772b150d3
hudson <hudson-watchers@newrelic.com>
2013-09-13 07:35:27 -0700

    Updates Azure Web Sites NuGet package to version 2.12.146.0

--- nuget.test/tests/package_content.Tests.ps1
+++ nuget.test/tests/package_content.Tests.ps1
@@ -4,7 +4,7 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
 		
-		$agentVersion = "2.10.40.0"
+		$agentVersion = "2.12.146.0"
 		
 		Context "When package exists" {
 			It "checks to see if content\newrelic.config exists" {

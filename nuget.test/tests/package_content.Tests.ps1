c93e5b2d5ca40ef3c1a5cf372862e89061d14f5e
hudson <hudson-watchers@newrelic.com>
2015-08-20 08:19:12 -0700

    Updates NewRelic.Azure.WebSites to version 5.3.90.0

--- nuget.test/tests/package_content.Tests.ps1
+++ nuget.test/tests/package_content.Tests.ps1
@@ -4,7 +4,7 @@ Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
     Describe "package_content" {
 		
-		$agentVersion = "5.2.87.0"
+		$agentVersion = "5.3.90.0"
 		
 		Context "When package exists" {
 			It "checks to see if content\newrelic.config exists" {
